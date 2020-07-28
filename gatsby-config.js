c50c16668bbdcc71755d4ae2175b9903585ebfb6
Craig Freeman <craigfreeman@gmail.com>
2020-07-27 22:42:01 -0600

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,20 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Wrangle Dangle`,
+        short_name: `WrangleDangle`,
+        start_url: `/`,
+        background_color: `#00696b`,
+        theme_color: `#000000`,
+        display: `standalone`,
+        icon: "static/assets/img/favicon.jpg",
+      },
+    },
+  ],
 }
